f18ef2a7d56bc4566940fcde510a8151f0ac9678
termanty <termanty@gmail.com>
2016-08-17 14:51:39 +0300

    WIP: create poll form

--- app/poll-creator/poll-creator.component.ts
+++ app/poll-creator/poll-creator.component.ts
@@ -1,12 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormControl, FormGroup, FormArray, FormBuilder } from '@angular/forms';
+import { Poll } from '../poll.model';
+import { PollService } from '../poll.service';
+
 @Component({
   selector: 'app-poll-creator',
   templateUrl: 'app/poll-creator/poll-creator.component.html'
 })
 export class PollCreatorComponent implements OnInit {
-  constructor() { }
+  form: FormGroup;
+  choices: FormArray;
+
+  constructor(
+    private pollService: PollService,
+    private _fb: FormBuilder) {
+    this.form = _fb.group({
+      title: 'trump vs hilary',
+      description: '',
+      choices: this.buildArray()
+    });
+  }
 
   ngOnInit() { }
 
+  buildGroup(): FormGroup {
+    return this._fb.group({
+      choice: ''
+    });
+  }
+
+  buildArray(): FormArray {
+    this.choices = this._fb.array([
+      this.buildGroup()
+    ]);
+    return this.choices;
+  }
+
+  addChoice() { this.choices.push(this.buildGroup()) }
+
+  savePoll() {
+    this.pollService.addPoll(
+      new Poll(
+        this.form.value['title'],
+        this.form.value['description'],
+        this.form.value['choices'],55)
+    );
+  }
 }
--- app/poll.service.ts
+++ app/poll.service.ts
@@ -5,26 +5,32 @@ import { Poll } from './poll.model';
 const POLLS = [
   new Poll('Clinton vs. Trump',
     'Title says all. Which one is your favourite?',
-    ['Clinton', 'Trump', 'Not interested']),
+    ['Clinton', 'Trump', 'Not interested'], 1),
   new Poll('CocaCola vs Pepsi',
     'Taste maters. Which one is your favourite?',
-    ['CocaCola', 'Pepsi', 'Not for me']),
+    ['CocaCola', 'Pepsi', 'Not for me'], 2),
   new Poll('Pasta vs Potatoes',
     'Taste maters. Which one is your favourite?',
-    ['Pasta', 'Potatoes', 'Not for me']),
+    ['Pasta', 'Potatoes', 'Not for me'], 3),
   new Poll('Milk vs Vine',
     'Taste maters. Which one is your favourite?',
-    ['Milk', 'Vine', 'I prefer water'])
+    ['Milk', 'Vine', 'I prefer water'], 4)
 ]
 
 @Injectable()
 export class PollService {
-  polls = POLLS;
+  polls: Poll[] = [];
 
-  getPolls() { return Promise.resolve(POLLS); };
+  constructor() { this.polls = POLLS }
+
+  getPolls() { return Promise.resolve(this.polls); };
 
   getPollsSlowly() { return new Promise<Poll[]>(
-    resolve => setTimeout( () => resolve(POLLS), 1000 )
+    resolve => setTimeout( () => resolve(this.polls), 1000 )
   )};
+
+  addPoll(p: Poll) {
+    this.polls.push(p);
+  }
 
 }
